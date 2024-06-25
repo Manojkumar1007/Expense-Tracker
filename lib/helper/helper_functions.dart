fb1643ea8003411d59362c7195f3bb00263252e3
Manoj Kumar Atmakuri <manojkumaratmakuri@gmail.com>
2024-06-26 00:38:04 +0530

    First commit

---
+++ lib/helper/helper_functions.dart
@@ -0,0 +1,35 @@
+import 'package:intl/intl.dart';
+
+double convertStringToDouble(String string) {
+  double? amount = double.tryParse(string);
+  return amount ?? 0 ;
+}
+
+String formatAmount(double amount) {
+  final format = NumberFormat.currency(name: "INR",locale: 'en_IN',decimalDigits: 2,symbol: '₹');
+  return format.format(amount);
+}
+
+int calculateMonthCount(int startYear, startMonth, currentYear, currentMonth){
+  int monthCount = (currentYear - startYear) * 12 + currentMonth - startMonth + 1;
+  return monthCount;
+}
+
+String getCurrentMonthName(){
+  DateTime now = DateTime.now();
+  List<String> months = [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC",
+  ];
+  return months[now.month - 1];
+}
